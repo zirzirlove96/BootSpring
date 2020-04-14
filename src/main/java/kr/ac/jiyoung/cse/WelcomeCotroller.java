0cb3a8d980ca337acc8c7e4a8ab7a453c2a1f827
zirzirlove96 <ovokjy4756@naver.com>
2020-04-14 14:02:28 +0900

    Second Lessond
    I used Mustache template engine.

---
+++ src/main/java/kr/ac/jiyoung/cse/WelcomeCotroller.java
@@ -0,0 +1,18 @@
+package kr.ac.jiyoung.cse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeCotroller {
+	
+	@GetMapping("/hello")
+	public String welcome(String name, int age, Model model) {//url queryString으로 온 값을 받아온다.
+		System.out.println(name+" "+age);
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		return "welcome";
+	}
+
+}

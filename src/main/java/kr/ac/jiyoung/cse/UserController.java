00c8b68524d7837495f8ce832de998c7c44ace6f
zirzirlove96 <ovokjy4756@naver.com>
2020-04-14 21:18:34 +0900

    Second Lesson
    User Register Page

--- src/main/java/kr/ac/jiyoung/cse/UserController.java
+++ src/main/java/kr/ac/jiyoung/cse/UserController.java
@@ -1,14 +1,20 @@
 package kr.ac.jiyoung.cse;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.ac.jiyoung.model.User;
 
 @Controller
 public class UserController {
+	
+	private List<User> users = new ArrayList<User>();
 	
 	@PostMapping("/create")
 	public String create(User user,Model model) {
@@ -17,7 +23,15 @@ public class UserController {
 		model.addAttribute("name", user.getName());
 		model.addAttribute("email", user.getEmail());
 		System.out.println(user);
-		return "index";
+		users.add(user);
+		return "redirect:/list";//redirect는 /list를 수행시키는 역할을 한다.
+	}
+	
+	@GetMapping("/list")
+	public String getList(Model model) {
+		model.addAttribute("users", users);//List에 저장된 값들을 
+		//list.html로 전달해 준다.
+		return "list";
 	}
 
 }
